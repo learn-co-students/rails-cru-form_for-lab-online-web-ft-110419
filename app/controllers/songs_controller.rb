ec88233ca19f6578196a234b26b1d687793e16d1
Bruno Zanin <corporate.zanin@gmail.com>
2020-01-15 21:32:42 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,33 @@
+class SongsController < ApplicationController
+
+  def index
+      @song = Song.all
+    end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(params.require(:song).permit(:name, :artist_id, :genre_id))
+    @song.save
+    redirect_to song_path(@song)
+  end
+
+
+  def show
+    @song = Song.find_by_id(params[:id])
+  end
+
+  def edit
+    @song = Song.find_by_id(params[:id])
+  end
+
+  def update
+    @song = Song.find_by_id(params[:id])
+    @song.update(params.require(:song).permit(:name))
+    redirect_to song_path(@song)
+  end
+
+
+end
